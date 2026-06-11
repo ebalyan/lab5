1a39fed3d5c81d8bbc78358e8f2f78454f680e25
ebalyan <ebalyan@usc.edu>
2026-06-11 16:21:47 -0700

    stones.cpp

--- stones.cpp
+++ stones.cpp
@@ -3,7 +3,23 @@
 
 int lastStoneWeight(std::vector<int>& stones) {
     MaxHeap<int> heap;
-    
-    // TO BE COMPLETED
-    
+
+    for (std::size_t i = 0; i < stones.size(); i++) {
+        head.push(stones[i]);
+    }
+    while(heap.size() > 1) {
+        int y = heap.top();
+        heap.pop();
+
+        int x = heap.top();
+        heap.pop();
+
+        if (x != Y) {
+            heap.push(y - x);
+        }
+    }
+    if (heap.size() == 0) {
+        return 0;
+    }
+    return heal.top();
 }
